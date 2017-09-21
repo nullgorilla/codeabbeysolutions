7f60e0dc69fe45fbb23daa83cc3bcc26d9abd37d
emre.ciklatekerlio <emre.ciklatekerlio@BLS-ECIKLA.ptt.local>
2017-09-21 14:38:18 +0300

    aa

---
+++ Deneme/src/deneme/Deneme.java
@@ -0,0 +1,13 @@
+package deneme;
+
+public class Deneme {
+
+	public Deneme() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("emre");
+	}
+
+}
