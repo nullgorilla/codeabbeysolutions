bf7782c3da19f74c3e56b01907836d2c87c4d318
emre.ciklatekerlio <emre.ciklatekerlio@BLS-ECIKLA.ptt.local>
2017-09-21 15:11:13 +0300

    sumloop

--- CodeAbbeySolutions/src/problems10/SumLoop.java
+++ CodeAbbeySolutions/src/problems10/SumLoop.java
@@ -1,9 +1,22 @@
 package problems10;
 
+import java.util.Scanner;
+
 public class SumLoop {
 
-	public SumLoop() {
-		// TODO Auto-generated constructor stub
-	}
-
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("input data:");
+        int size = input.nextInt();
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            result += input.nextInt();
+        }
+        
+        input.close();
+        
+        System.out.println(result);
+        
+    }
 }
