63bdf13b04724575cd5f417ff1b7f788eb9f599c
emre.ciklatekerlio <emre.ciklatekerlio@BLS-ECIKLA.ptt.local>
2017-10-19 16:42:19 +0300

    problems 60

---
+++ CodeAbbeySolutions/src/problems60/BlackjackCounting.java
@@ -0,0 +1,48 @@
+package problems60;
+
+import java.util.Scanner;
+
+public class BlackjackCounting {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int size = input.nextInt();
+		
+		input.nextLine();
+		for (int i = 0; i < size; i++) {
+			Scanner line = new Scanner(input.nextLine());
+			int point = 0;
+			while (line.hasNext()) {
+				String card = line.next();
+				int cardValue = 0;
+
+				switch (card) {
+				case "K":
+				case "Q":
+				case "J":
+				case "T":
+					cardValue = 10;
+					break;
+				case "A":
+					if(point + 11 < 21) {
+						cardValue = 11;
+					}else {
+						cardValue = 1;
+					}
+					break;
+				default:
+					cardValue = Integer.parseInt(card);
+					break;
+				}
+				point += cardValue;
+			}
+			if(point > 21) {
+				System.out.print("Bust ");
+			}else {
+				System.out.print(point+" ");
+			}
+			line.close();
+		}
+		input.close();
+	}
+}
